25c7dd65c292cd7ac955a0d9559961417f1f6b42
Alfredo Hernández Rodríguez <Geekahr@MacBook-Pro-de-Alfredo.local>
2016-08-04 21:26:42 -0500

    Primer app /inicio

--- main/models.py
+++ main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+##class Registrado(models.Model):
+##	nombre = models.CharField(max_length=120,blank=True,null=True)
+##	email = models.EmailField()
+##	codigo_postal = models.IntegerField()
+##	timestap = models.DateTimeField(auto_now_add=True,auto_now=True)
+##	actualizado = models.DateTimeField(auto_now_add=True, auto_now=True)
+
+ ## def __str__ (self):
+##  	return self.email
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import View
+
+class MainView(View):
+	def get(self,request):
+		template_name = 'inicio.html'
+		return render(request,template_name)
+
 
 # Create your views here.
+
